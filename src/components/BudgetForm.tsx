18af53ab2a99ea36e9e0d465511a6ae58d0fa9f9
Everth Paredes <epe0413@gmail.com>
2024-04-22 10:23:08 -0600

    Installing Headless UI to have a modal window

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,7 +1,9 @@
-import { ChangeEvent, useMemo, useState } from "react"
+import { ChangeEvent, FormEvent, useMemo, useState } from "react"
+import { useBudget } from "../hooks/useBudget";
 
 export default function BudgetForm() {
     const [budget, setBudget] = useState(0)
+    const { dispatch } = useBudget()
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setBudget(e.target.valueAsNumber);
@@ -11,9 +13,16 @@ export default function BudgetForm() {
         return isNaN(budget) || budget <= 0
     }, [budget])
 
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        dispatch({type: 'add-budget', payload: { budget } })
+    }
+
     return (
         <form
             className="space-y-5"
+            onSubmit = {handleSubmit}
         >
             <div className="flex flex-col space-y-5">
                 <label htmlFor="budget" className="text-2xl text-blue-600 font-bold text-center">
